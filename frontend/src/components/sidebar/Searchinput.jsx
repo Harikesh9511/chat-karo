52eef823c8863439b97c3ead44cc1df7b769acb9
Harikesh9511 <harikeshmishra9511@gmail.com>
2025-09-05 08:34:16 +0530

    2nd part completed -ui design

---
+++ frontend/src/components/sidebar/Searchinput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { IoSearchSharp } from "react-icons/io5";
+const Searchinput = () => {
+  return (
+    <form className="flex items-center">
+      <input
+        type="text"
+        placeholder="Search"
+        className="input input-bordered rounded-full"
+      />
+
+      <button
+        type="submit"
+        className="btn btn-circle bg-sky-500 text-white"
+      ><IoSearchSharp className=' w-6 h-6 outline-none'></IoSearchSharp></button>
+    </form>
+  );
+};
+
+export default Searchinput;
