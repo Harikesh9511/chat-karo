8eae35d62c2daebcc01d662111990266cb177afe
Harikesh9511 <harikeshmishra9511@gmail.com>
2025-09-10 00:59:44 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 
 import connectToMongoDB from "./db/connectToMongoDb.js";
-
-
-const app = express();
-const PORT = process.env.PORT || 5001;
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+
+const PORT = process.env.PORT || 5001;
+
+
+
 app.use(express.json());
 app.use(cookieParser()); //so that we can use cookie for route protection
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
@@ -25,7 +29,7 @@ app.use("/api/users", userRoutes);
 
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on Port ${PORT}`)
 });
---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { createContext } from "react";
+import { useAuthContext } from "./AuthContext";
+import { useEffect } from "react";
+import io from "socket.io-client";
+import { useContext } from "react";
+
+ const SocketContext = createContext();
+
+ export const useSocketContext = () => {
+  //this export for using anywhere in app
+  return useContext(SocketContext);
+ }
+
+export const SocketContextProvider = ({ children }) => {
+  //this export for main.jsx
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
+  // these things we can use anywhere
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+
+      setSocket(socket);
+
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
